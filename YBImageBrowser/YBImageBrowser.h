9cf4b75d69944dfbd729c9aae1837d5ac892a363
NealWills <nealwills93@gmail.com>
2023-03-19 16:49:26 +0800

    3.1.4 fix a bug

--- YBImageBrowser/YBImageBrowser.h
+++ YBImageBrowser/YBImageBrowser.h
@@ -70,8 +70,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// 预加载数量 (默认为 2，低内存设备默认为 0)
 @property (nonatomic, assign) NSUInteger preloadCount;
 
-@property (nonatomic, weak) void(^browserDidShowBlock)(void);
-@property (nonatomic, weak) void(^browserDidDismissBlock)(void);
+@property (nonatomic, copy) void(^browserDidShowBlock)(void);
+@property (nonatomic, copy) void(^browserDidDismissBlock)(void);
 
 /**
  重载数据，请保证数据源被正确修改
